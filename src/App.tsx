acfaae6fea4fa8a7ca25c8fe958c71ae7fe0b4b6
John Griebel <jkgriebel93@gmail.com>
2026-04-28 20:55:00 -0400

    feat(TGF-325): Add theme-preview
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import "@mantine/dates/styles.css";
 
 import { AppShell, Burger, Group, MantineProvider, Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -7,43 +8,48 @@ import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 
 import { ThemeToggle } from "./components/ThemeToggle";
+import { ThemePreviewPage } from "./pages/ThemePreview.page";
 
 export function App() {
 	const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
 	const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
 
+	const isThemePreview =
+		typeof window !== "undefined" && window.location.pathname === "/theme-preview";
+
 	return (
 		<MantineProvider defaultColorScheme="auto">
 			<Notifications />
-			<ModalsProvider />
-			<AppShell
-				padding="md"
-				header={{ height: 48 }}
-				navbar={{
-					width: 300,
-					breakpoint: "sm",
-					collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
-				}}
-			>
-				<AppShell.Header>
-					<Group h="100%" px="md" justify="space-between">
-						<Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-						<Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-						<Group>
-							<Title order={3}>Griddy Web Portal</Title>
+			<ModalsProvider>
+				<AppShell
+					padding="md"
+					header={{ height: 48 }}
+					navbar={{
+						width: 300,
+						breakpoint: "sm",
+						collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+					}}
+				>
+					<AppShell.Header>
+						<Group h="100%" px="md" justify="space-between">
+							<Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+							<Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+							<Group>
+								<Title order={3}>Griddy Web Portal</Title>
+							</Group>
+							<Group>
+								<ThemeToggle />
+							</Group>
 						</Group>
-						<Group>
-							<ThemeToggle />
-						</Group>
-					</Group>
-				</AppShell.Header>
+					</AppShell.Header>
 
-				<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+					<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
 
-				<AppShell.Main>
-					<Text>Hello world!</Text>
-				</AppShell.Main>
-			</AppShell>
+					<AppShell.Main>
+						{isThemePreview ? <ThemePreviewPage /> : <Text>Hello world!</Text>}
+					</AppShell.Main>
+				</AppShell>
+			</ModalsProvider>
 		</MantineProvider>
 	);
 }
