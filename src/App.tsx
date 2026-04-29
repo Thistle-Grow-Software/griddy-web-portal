8f752eb5ac77380af5c6c8dda26174e17591602f
John Griebel <jkgriebel93@gmail.com>
2026-04-28 20:49:01 -0400

    feat(TGF-325): Add notifications system, modals provider, navbar open/close
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
-import { AppShell, Burger, HoverCard, MantineProvider } from "@mantine/core";
+import { AppShell, Burger, Group, MantineProvider, Text, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
 
 import { ThemeToggle } from "./components/ThemeToggle";
-import Group = HoverCard.Group;
 
 export function App() {
-	const [opened, { toggle }] = useDisclosure();
+	const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+	const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
 
 	return (
 		<MantineProvider defaultColorScheme="auto">
+			<Notifications />
+			<ModalsProvider />
 			<AppShell
 				padding="md"
-				header={{ height: 60 }}
+				header={{ height: 48 }}
 				navbar={{
 					width: 300,
 					breakpoint: "sm",
-					collapsed: { mobile: !opened },
+					collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
 				}}
 			>
 				<AppShell.Header>
 					<Group h="100%" px="md" justify="space-between">
+						<Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+						<Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
 						<Group>
-							<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-							<span>Griddy</span>
+							<Title order={3}>Griddy Web Portal</Title>
 						</Group>
-						<ThemeToggle />
+						<Group>
+							<ThemeToggle />
+						</Group>
 					</Group>
 				</AppShell.Header>
 
 				<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
 
-				<AppShell.Main>Main</AppShell.Main>
+				<AppShell.Main>
+					<Text>Hello world!</Text>
+				</AppShell.Main>
 			</AppShell>
 		</MantineProvider>
 	);
---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,5 @@
+import { Container } from "postcss";
+
+export function HomePage() {
+	return <Container />;
+}
